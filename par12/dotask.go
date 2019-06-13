319754de4f1a9bbdd8343e07af5343e43b5dc7e4
daheige <zhuwei313@hotmail.com>
2019-06-13 21:43:05 +0800

    生成100w个数字并打乱顺序

--- par12/dotask.go
+++ par12/dotask.go
@@ -5,9 +5,18 @@ package main
 */
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/daheige/thinkgo/common"
 )
+
+//产生[0-n]之间的[]int
+func shuffle(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	b := rand.Perm(n)
+	return b
+}
 
 func main() {
 	//安全的执行goroutine
@@ -24,4 +33,17 @@ func main() {
 	log.Println("res: ", res)
 	log.Println("exec time: ", res.CostTime)
 	log.Println(res.Err == nil, <-res.Result == nil)
+
+	var x = 100 * 10000 //产生100w个数字打乱顺序
+	res2 := common.DoTaskWithArgs(func(args ...interface{}) interface{} {
+		n := args[0].(int)
+
+		log.Println(shuffle(n))
+
+		return nil
+	}, x)
+
+	//错误信息都可以抛出来
+	log.Println("cost time: ", res2.CostTime) //cost time:  0.382877745
+	log.Println(res2.Err, <-res2.Result)
 }
