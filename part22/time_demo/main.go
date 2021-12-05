7b14fb6adbbdb88cbaf678b56b13bbb0fb60f773
daheige <zhuwei313@hotmail.com>
2021-12-05 20:54:35 +0800

    goroutine timeout exit signal

--- part22/time_demo/main.go
+++ part22/time_demo/main.go
@@ -13,13 +13,20 @@ func main() {
 	// 		time.Sleep(200 * time.Millisecond)
 	// 	}
 	// }, 1*time.Second)
-
+	exit := make(chan struct{}, 1)
 	AsyncCallByCtx(func() {
 		for i := 0; i < 100; i++ {
+			select {
+			case <-exit:
+				log.Println("exit func call")
+				return
+			default:
+			}
+
 			log.Println("current index2: ", i)
 			time.Sleep(300 * time.Millisecond)
 		}
-	}, 3*time.Second)
+	}, 3*time.Second, exit)
 	time.Sleep(2 * time.Second)
 }
 
@@ -45,7 +52,7 @@ func AsyncCall(fn func(), timeout time.Duration) {
 }
 
 // AsyncCallByCtx 通过ctx timeout方式实现fn调度，超时就取消fn执行
-func AsyncCallByCtx(fn func(), timeout time.Duration) {
+func AsyncCallByCtx(fn func(), timeout time.Duration, exit chan struct{}) {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "abc", 123)
 	ctx, cancel := context.WithTimeout(ctx, timeout)
@@ -62,6 +69,9 @@ func AsyncCallByCtx(fn func(), timeout time.Duration) {
 			cancel()
 		}
 
+		close(exit) // 退出信号量,用来通知fn func函数中断执行，这里仅仅让fn func for逻辑不再执行了
+
+		// 当上下文被取消后，Value是可以拿到ctx数据
 		log.Println("abc: ", ctx.Value("abc")) // abc:  123 这里从上下文获取key
 		log.Println("timeout")
 	case <-done:
